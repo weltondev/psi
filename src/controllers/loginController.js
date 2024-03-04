e2c7c1ed99cc560b065b8d8b3f04fc60283d678d
weltonDev <welton.developer95@gmail.com>
2024-03-04 18:24:45 -0300

    teste

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -15,13 +15,13 @@ const loginController = {
         return res.status(400).send(`Credenciais inválidas`);
       }
 
-      // const token = jwt.sign({
-      //   id: usuario._id,
-      //   nome: usuario.nome,
-      //   email: usuario.email
-      // }, 'univesp', { expiresIn: '1h' });
+      const token = jwt.sign({
+        id: usuario._id,
+        nome: usuario.nome,
+        email: usuario.email
+      }, 'univesp', { expiresIn: '1h' });
       
-      res.status(200).json({ passou });
+      res.status(200).json({msg: 'Autenticado', token})
     } catch (error) {
       console.log(error);
       res.status(400).send(`Falha ao realizar login ${error}`);
