9c326b439cd90b6791a8387a92ebc524ae2ce1ed
weltonDev <welton.developer95@gmail.com>
2024-03-04 18:23:08 -0300

    teste

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -21,7 +21,7 @@ const loginController = {
       //   email: usuario.email
       // }, 'univesp', { expiresIn: '1h' });
       
-      res.status(200).json(usuario);
+      res.status(200).json({ passou });
     } catch (error) {
       console.log(error);
       res.status(400).send(`Falha ao realizar login ${error}`);
